8620368ca85a2a597ed49a43717369905e5040e1
Brett Markowitz <batting1000@comcast.net>
2018-06-01 00:09:55 -0400

    Basic functions (add and delete), removed change function, added options menu

--- todo.py
+++ todo.py
@@ -1,7 +1,8 @@
 todos = []
+waiting = True
 
 def display_todos():
-    print("Todo List: ")
+    print("\n#####\nTodo List: \n")
     if len(todos) == 0:
         print('You have no todos.')
     else:
@@ -10,24 +11,39 @@ def display_todos():
                 print('(x) ' + todo['text'])
             else:
                 print('( ) ' + todo['text'])
+    print('#####\n')
 
-def add_todo(todo_text):
-    todos.append({
-        'text': todo_text,
-        'completed': False,
-    })
-    display_todos()
+def add_todo():
+    todo_text = input('Enter a todo: ')
+    if todo_text:
+        todos.append({
+            'text': todo_text,
+            'completed': False,
+        })
+    else:
+        print('** Invalid input. Try again. ** ')
 
-def delete_todo(index):
-    del todos[index]
-    display_todos()
-
-def change_todo(index, new_todo_text):
+def delete_todo():
     try:
-        todos[index]['text'] = new_todo_text
-    except IndexError:
-        print('The todo you have tried to change does not exist.')
-    display_todos()
+        todo_to_delete = int(input('Enter the number of the todo to delete: ')) - 1
+        del todos[todo_to_delete]
+    except (ValueError, IndexError):
+        print('** Invalid input. Try again. **')
 
-add_todo('hello')
-change_todo(0, 'goodbye')
+while(waiting):
+    display_todos()
+    print('Menu:\n- Add todo: 1')
+    print('- Delete todo: 2')
+    print('- Exit: 4')
+
+    try:
+        option = int(input('** Select an option: '))
+        if option:
+            if option == 1:
+                add_todo()
+            elif option == 2:
+                delete_todo()
+            elif option == 4:
+                waiting = False
+    except ValueError:
+        print('** Invalid input. Try again. **\n')
